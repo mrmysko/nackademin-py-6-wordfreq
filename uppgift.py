931c9acccb04b280bc281140952c755602074f18
Robin Kavenius <robin.kavenius@yh.nackademin.se>
2024-03-06 07:12:26 +0100

    Wordfreq

--- uppgift.py
+++ uppgift.py
@@ -1,5 +1,20 @@
 # Skriv endast din funktionsdefinition här på denna indenteringsnivå! Det är
 # viktigt att du ger funktionen exakt det namn som står i beskrivningen.
+def wordfreq(words: str) -> dict:
+    """Returns a dict with wordfrequency"""
+    list_words = words.split()
+    dict_words = {}
+
+    for i in list_words:
+        if i not in dict_words:
+            dict_words[i] = 1
+        else:
+            dict_words[i] += 1
+
+    # dict_words = {[(key, value) for key, in list_words]}
+
+    return dict_words
+
 
 if __name__ == "__main__":
     # Här kan du skriva testkod för din funktion. Denna körs endast när du kör
@@ -12,4 +27,7 @@ if __name__ == "__main__":
     #
     # print(funktionsnamn("hejsan", 99))
     # print(funktionsnamn([19, 22, 31, 29, 1])
-    pass  # Ta bort denna rad
+    print(wordfreq("python programmering python"))
+    print(wordfreq("test test test"))
+    print(wordfreq("hej hej på dig"))
+    print(wordfreq(""))
